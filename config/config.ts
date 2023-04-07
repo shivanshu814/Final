0f94e9c2ad1305a0ca99dac1aa7f84a791e782c5
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-04-08 01:17:18 +0530

    Firebase Authentication log

---
+++ config/config.ts
@@ -0,0 +1,15 @@
+/* @format */
+
+const config = {
+	firebase: {
+		apiKey: 'AIzaSyDD1YQSlXMPqEneR6mYwxeKBfoq5s79Vps',
+		authDomain: 'fir-auth-1132-12dbd.firebaseapp.com',
+		projectId: 'fir-auth-1132-12dbd',
+		storageBucket: 'fir-auth-1132-12dbd.appspot.com',
+		messagingSenderId: '854471250247',
+		appId: '1:854471250247:web:d3f30c481d970e1537fc62',
+		measurementId: 'G-4CR3VFMCWJ',
+	},
+};
+
+export default config;
